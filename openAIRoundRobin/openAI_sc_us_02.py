3b60a26956272e5f588286fb1f7a35e3aa02f5b8
huqianghui <huqianghui0613@163.com>
2023-08-18 16:26:44 +0800

    Update openAI_sc_us_02.py

--- openAIRoundRobin/openAI_sc_us_02.py
+++ openAIRoundRobin/openAI_sc_us_02.py
@@ -3,7 +3,7 @@ from langchain.chat_models import AzureChatOpenAI
 
 
 openaiscus02.api_type = "azure"
-openaiscus02.api_key = "9f5af36f5ea34e49a2bbf44b75e2660e"
+openaiscus02.api_key = "XXX"
 openaiscus02.api_base = "https://openai-demo-sc2.openai.azure.com/"
 openaiscus02.api_version = "2023-05-15"
 
