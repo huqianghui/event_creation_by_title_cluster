1ee77f5f59cf236e817293edeff3511e9a9a1555
huqianghui <huqianghui0613@163.com>
2023-08-18 16:25:38 +0800

    Update openAI_east_us_03.py

--- openAIRoundRobin/openAI_east_us_03.py
+++ openAIRoundRobin/openAI_east_us_03.py
@@ -3,7 +3,7 @@ from langchain.chat_models import AzureChatOpenAI
 
 
 openaieastus03.api_type = "azure"
-openaieastus03.api_key = "b3813d19476f464982ae9d6b6022c0ac"
+openaieastus03.api_key = "XXXX"
 openaieastus03.api_base = "https://openai-demo3-east-us.openai.azure.com/"
 openaieastus03.api_version = "2023-05-15"
 
